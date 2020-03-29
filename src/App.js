7c7fe83c351187543a462a6f23cf34447532c509
maria140p <62843108+maria140p@users.noreply.github.com>
2020-03-29 18:35:12 +0300

    New component - Logos

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./styles.css";
+import Logos from "./components/logos";
 
 export default function App() {
   return (
     <div className="App">
       <h1>Corona SMS Greece</h1>
       <h2>Prepare SMS for Lockdown Information</h2>
+      <Logos />
     </div>
   );
 }
---
+++ src/components/logos.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Logos() {
+  return (
+    <div className="logoscl">
+      <h4>Λόγος</h4>
+      <select>
+        <option value="1">Μετάβαση στό Φαρμακείο</option>
+      </select>
+    </div>
+  );
+}
