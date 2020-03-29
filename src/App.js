eb5b248cba270caea959cb3138e957ad73e96ac9
maria140p <62843108+maria140p@users.noreply.github.com>
2020-03-29 18:22:52 +0300

    New Titles
    Changed titles and colors

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Corona SMS Greece</h1>
+      <h2>Prepare SMS for Lockdown Information</h2>
     </div>
   );
 }
