acbced4e6ea0c21ede2acce63acac35692b1ae77
maria140p <62843108+maria140p@users.noreply.github.com>
2020-03-29 19:24:16 +0300

    component addres

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./styles.css";
 import Logos from "./components/logos";
 import Onoma from "./components/onoma";
+import Addres from "./components/addres";
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
       <h2>Prepare SMS for Lockdown Information</h2>
       <Logos />
       <Onoma />
+      <Addres />
     </div>
   );
 }
---
+++ src/components/addres.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Onoma() {
+  return (
+    <div className="addrescl">
+      <h4>Διεύθηνση</h4>
+      <input
+        type="text"
+        id="fname"
+        name="fname"
+        value=""
+        placeholder="π.χ. Αριστοτέλους 11, Θεσσαλονίκη"
+      />
+    </div>
+  );
+}
